73a8c61f89a7dc5066a924017b42590cbbbc8a54
illumination-k <illumination.k.27@gmail.com>
2021-02-08 13:24:38 +0900

    add makers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::process;
 use std::error::Error;
+use std::process;
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
--- test.sh
+++ test.sh
@@ -15,8 +15,6 @@ assert() {
   fi
 }
 
-cargo build
-
 assert 0 0
 assert 42 42
 
