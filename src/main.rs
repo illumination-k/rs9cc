e6d9db8b06ead870df476171f282d3b4da1b3046
illumination-k <illumination.k.27@gmail.com>
2021-02-08 13:10:12 +0900

    return code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+use std::error::Error;
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("引数の個数が正しくありません");
+        process::exit(1);
+    }
+    let formula = args[1].clone();
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", formula);
+    println!("  ret");
+    Ok(())
 }
