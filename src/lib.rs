eadf5df40e005ac729e06d423b547dfaee695ae4
illumination-k <illumination.k.27@gmail.com>
2021-02-09 20:27:52 +0900

    fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /*!
-The purpose of this crate is to make **C** cmmpiler by rust with reference to [9cc](www.sigbus.info/compilerbook)
+The purpose of this crate is to make **C** compiler by rust with reference to [9cc](www.sigbus.info/compilerbook)
 */
 
 pub mod node;
